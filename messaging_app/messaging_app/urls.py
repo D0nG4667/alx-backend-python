c4e370c234b340fb690013479ae60ad83b616ebc
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-11-16 18:27:10 +0300

    feat(routing): configure DRF router for conversations and messages under unified /api path
    - Registered `ConversationViewSet` and `MessageViewSet` using `DefaultRouter` in `chats/urls.py`
    - Included chat routes in main `messaging_app/urls.py` under `/api/` namespace
    - Enabled automatic RESTful endpoints for listing and creating conversations and messages
    
    This commit completes the API routing layer and prepares the backend for authentication, pagination, and documentation.

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from drf_yasg import openapi
 
 # drf-yasg
@@ -25,31 +25,31 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="ALX Travel App API",
-        default_version="v1",
-        description="API documentation for ALX Travel App",
+        title='ALX Travel App API',
+        default_version='v1',
+        description='API documentation for ALX Travel App',
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # path("api/", include("chats.urls")),  # chats app urls here
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),  # chats app urls here
     # swagger / redoc
     re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
+        r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
+        name='schema-json',
     ),
     path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
     ),
     path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
     ),
 ]
