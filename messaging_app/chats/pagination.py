167742641274b8f4b014828a96dd851b8a64ae03
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-11-23 14:26:20 +0300

    fix: checker req:

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -9,3 +10,15 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                'count': self.page.paginator.count,  # total items
+                'num_pages': self.page.paginator.num_pages,  # total pages
+                'current_page': self.page.number,
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'results': data,
+            }
+        )
